d49dae0f048a1ddc8604305874d2b9abf7fcbaa4
vlorian <vlorian.dervishi@gmail.com>
2023-07-14 10:11:21 -0400

    cleaned up csv and streamed using forEach loop

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+getData(); // calls getData() function
+
 async function getData() {
-  const response = await fetch("test.csv");
-  const data = await response.json();
-  return data;
+  const response = await fetch("./ZonAnn.Ts+dSST.csv");
+  const table = await response.text();
+  const row = table.split("\n").splice(1);
+  row.forEach((rws) => {
+    const rows = rws.split(",");
+    const year = rows[0];
+    const temp = rows[1];
+    console.log(year, temp);
+  });
 }
