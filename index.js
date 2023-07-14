e5bdf1663d07f6ffc3567c9ca9e34b14e5625130
vlorian <vlorian.dervishi@gmail.com>
2023-07-14 17:45:29 -0400

    created Async function chart invocted within getdata

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-getData(); // calls getData() function
+document.addEventListener("DOMContentLoaded", () => {
+  async function getData() {
+    const response = await fetch("./ZonAnn.Ts+dSST.csv");
+    const table = await response.text();
+    const row = table.split("\n").splice(1);
+    const array = [];
+    row.forEach((rws) => {
+      const rows = rws.split(",");
+      const year = rows[0];
+      const temp = rows[1];
+      // get array of objects and push to array to data set array on chart.js
+      //console.log(year, temp);
+      array.push({
+        year: year,
+        temp: temp,
+      });
+    });
+    getChart(array);
+  }
 
-async function getData() {
-  const response = await fetch("./ZonAnn.Ts+dSST.csv");
-  const table = await response.text();
-  const row = table.split("\n").splice(1);
-  row.forEach((rws) => {
-    const rows = rws.split(",");
-    const year = rows[0];
-    const temp = rows[1];
-    // get array of objects and push to array to data set array on chart.js
-    //console.log(year, temp);
-    return year, temp;
-  });
-  return row;
-}
+  getData();
+  async function getChart(data) {
+    new Chart(document.getElementById("canvas"), {
+      type: "bar",
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: "Acquisitions by year",
+            data: data.map((row) => row.temp),
+          },
+        ],
+      },
+    });
+  }
+});
+//import Chart from "chart.js/auto";
